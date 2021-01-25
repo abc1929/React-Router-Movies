e96516d59637bc65ab86ad6d4b98e241c0794d01
Jiawei Wu <abc1929@gmail.com>
2021-01-26 04:52:41 +0800

    Basics and majority of the stretch

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,54 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useRouteMatch } from "react-router-dom";
 
 export default function Movie(props) {
-  const [movie, setMovie] = useState();
+   const [movie, setMovie] = useState();
 
-  let id = 1;
-  // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
+   let id = useRouteMatch().url.split("/").pop();
+   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
 
-  useEffect(() => {
-    axios
-      .get(`http://localhost:5000/api/movies/${id}`) // Study this endpoint with Postman
-      .then(response => {
-        // Study this response with a breakpoint or log statements
-        // and set the response data as the 'movie' slice of state
-      })
-      .catch(error => {
-        console.error(error);
-      });
-    // This effect should run every time time
-    // the `id` changes... How could we do this?
-  }, []);
+   // or use useParams()
 
-  // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = evt => { }
+   useEffect(() => {
+      axios
+         .get(`http://localhost:5000/api/movies/${id}`) // Study this endpoint with Postman
+         .then((response) => {
+            // Study this response with a breakpoint or log statements
+            // and set the response data as the 'movie' slice of state
+            // console.log(response);
+            setMovie(response.data);
+         })
+         .catch((error) => {
+            console.error(error);
+         });
+      // This effect should run every time time
+      // the `id` changes... How could we do this?
+   }, [id]);
 
-  if (!movie) {
-    return <div>Loading movie information...</div>;
-  }
+   // Uncomment this only when you have moved on to the stretch goals
 
-  const { title, director, metascore, stars } = movie;
+   // const saveMovie = evt => { }
+   //* this function is written inline below
 
-  return (
-    <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
+   if (!movie) {
+      return <div>Loading movie information...</div>;
+   }
 
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
+   const { title, director, metascore, stars } = movie;
+   const { saved, setSaved } = props;
+
+   return (
+      <div className="save-wrapper">
+         <div className="movie-card">
+            <h2>{title}</h2>
+            <div className="movie-director">
+               Director: <em>{director}</em>
+            </div>
+            <div className="movie-metascore">
+               Metascore: <strong>{metascore}</strong>
+            </div>
+            <h3>Actors</h3>
+
+            {stars.map((star) => (
+               <div key={star} className="movie-star">
+                  {star}
+               </div>
+            ))}
+         </div>
+         <div
+            className="save-button"
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+               if (!saved.find((i) => i.id === movie.id)) {
+                  setSaved(saved.concat(movie));
+               }
+            }}
+         >
+            Save
+         </div>
       </div>
-      <div className="save-button">Save</div>
-    </div>
-  );
+   );
 }
